2827f6fc61eb6df11101c3c4e73ce10ad3a67c8d
Joseeptessele <josetesselep@gmail.com>
2024-06-02 18:57:47 -0300

    creating request e response structs

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	rest_err "github.com/Joseeptessele/go-user-crud/src/configuration"
+	"github.com/Joseeptessele/go-user-crud/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(err.Error())
+
+		c.JSON(int(restErr.Code), restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 
 }
