5f03f22d5625ff78986fab7740795b0c5a6583bc
Joseeptessele <josetesselep@gmail.com>
2024-06-04 00:50:09 -0300

    configuring logs

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,34 +1,38 @@
 package controller
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
+	"github.com/Joseeptessele/go-user-crud/src/configuration/logger"
 	"github.com/Joseeptessele/go-user-crud/src/configuration/validation"
 	"github.com/Joseeptessele/go-user-crud/src/controller/model/request"
 	"github.com/Joseeptessele/go-user-crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller", zap.String("journey", "createUser"))
+
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("error trying to marshal object, error=%s\n", err.Error())
+		logger.Error("error trying to validate user info", err, zap.String("journey", "createUser"))
+
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
 
-	fmt.Println(userRequest)
 	response := response.UserResponse{
 		ID:    "test",
 		Email: userRequest.Email,
 		Name:  userRequest.Name,
 		Age:   userRequest.Age,
 	}
+
+	logger.Info("user created successfully", zap.String("journey", "createUser"))
 
 	c.JSON(http.StatusCreated, response)
 }
